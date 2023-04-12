3e8a2adb40e4f2c56da342b6fcf36914a3eb6ed1
Basak <chgy2131@naver.com>
2023-04-12 14:17:02 +0900

    GenericApplicationContext에서 중복 빈이 있는 경우 확인

--- hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/learn/SimpleHelloService.java
+++ hyeonsik/spring-boot-project/src/main/java/tobySpringBoot/learn/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package tobySpringBoot.learn;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name) {
